22d91b70b00ea58c22bff64e60bb15d8d5c3eb09
Rishi14101995 <137778217+Rishi14101995@users.noreply.github.com>
2023-09-12 19:42:03 +0000

    files updated

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,14 @@
+const accountId = 14456
+let accountEmail = "rishiparihar.parihar@gmail.com"
+var accountPassword  = "1234"
+accountCity = "Noida"
+let accountState;
+
+//accountId = 123 //not allowed
+/*
+Prefer not to use var keyword because it will issue in block scope and functional scope.
+*/
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword , accountCity, accountState]);
