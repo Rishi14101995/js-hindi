f63aedc44f00b216872ded7d307bcd8ddd735afb
Rishi14101995 <137778217+Rishi14101995@users.noreply.github.com>
2023-09-18 13:08:03 +0000

    2 Files added

---
+++ 01_basics/06_nums_math.js
@@ -0,0 +1,18 @@
+/******************************* Numbers *************************************************/
+const score = 50
+console.log(score);
+
+const balance = new Number(500)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(3));
+
+const otherNumber = 23.8996
+console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+/**********************************8 MATHS***********************************************/
